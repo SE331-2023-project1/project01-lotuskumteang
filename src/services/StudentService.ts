94a02305c4358a3b65e6adad224ea1d35d539ba6
Thai <thaiphat_s@cmu.ac.th>
2023-08-17 17:43:00 +0700

    Add StudentService.ts
    changed from passenger -> student

---
+++ src/services/StudentService.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import type { AxiosInstance, AxiosResponse } from 'axios'
+import type { StudentItem} from '@/type'
+
+const apiClient: AxiosInstance = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/se331-2022/project01-lotuskumteang',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getStudentItems(perPage: number, page: number): Promise<AxiosResponse<StudentItem[]>> {
+    return apiClient.get<StudentItem[]>('/students?_page=' + page + '&_limit=' + perPage)
+  },
+  getStudentItemById(id: number): Promise<AxiosResponse<StudentItem>> {
+    return apiClient.get<StudentItem>('/students/' + id.toString())
+  },
+  // getAirlineById(id: number): Promise<AxiosResponse<StudentItem>> {
+  //   return apiClient.get<StudentItem>('airline/' + id.toString())
+  // }
+}
