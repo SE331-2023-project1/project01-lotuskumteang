aac234ea50a9ea58f8940d575fddf4e37abb4da1
Thai <thaiphat_s@cmu.ac.th>
2023-08-18 18:02:33 +0700

    Add StudentDetailView

---
+++ src/views/details/StudentDetailView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { type StudentItem } from '@/type'
+import type { PropType } from 'vue'
+
+defineProps({
+  student: {
+    type: Object as PropType<StudentItem>,
+    require: true
+  }
+})
+</script>
+
+<template>
+  <div v-if="student">
+    <div class="card-wrapper">
+      <div class="card-body">
+        <h2>{{ student?.name }} {{ student?.surname }}</h2>
+        <p><span>ID:</span> {{ student?.studentId }}</p>
+        <img src="{{ student?.image }}" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card-body {
+  line-height: 36pt;
+  margin-top: 2rem;
+}
+span {
+  font-weight: bold;
+}
+</style>
