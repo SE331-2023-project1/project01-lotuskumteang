1f0acca07a470cffb1b57001c46ff1ec4f2120db
Thai <thaiphat_s@cmu.ac.th>
2023-08-18 17:52:33 +0700

    Add StudentLayoutView

---
+++ src/views/details/StudentLayoutView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useStudentStore } from '@/stores/student'
+import { storeToRefs } from 'pinia'
+
+const useStudent = useStudentStore()
+const student = storeToRefs(useStudent).student
+const id = ref(student?.value?.studentId)
+</script>
+
+<template>
+  <div v-if="student">
+    </div>
+    <RouterView :student="student"></RouterView>
+</template>
+
+<style scoped>
+.nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3rem;
+}
+.nav a.router-link-exact-active {
+  color: #42b983;
+  font-weight: bold;
+}
+
+.nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+.nav a {
+  display: inline-block;
+  padding: 0 0.5rem;
+  color: #2c3e50;
+}
+</style>
+@/stores/student
