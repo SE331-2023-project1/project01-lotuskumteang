832ca232510362992735f501fc0d135b7299309a
Thai <thaiphat_s@cmu.ac.th>
2023-08-18 20:01:53 +0700

    Add CommentForm

---
+++ src/components/CommentForm.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+const form = reactive({
+    content: ''
+})
+const emit = defineEmits(['comment-submit'])
+function onSubmit() {
+    const comment = {
+        content: form.content
+    }
+    if (form.content === '') {
+        alert('Comment is empty.')
+        return
+    }
+    emit('comment-submit', comment)
+    form.content = ''
+}
+</script>
+
+<template>
+    <form class="comment-form" @submit.prevent="onSubmit">
+        <label for="comment">Leave comment as advisor:</label>
+        <textarea id="comment" v-model="form.content"></textarea>
+        <input class="button" type="submit" value="Submit">
+    </form>
+</template>
