60864b557c46bcceedcb3e7feb6e597d40b35835
Taninwat <fame5454@gmail.com>
2023-08-19 16:18:42 +0700

    Create next page

--- src/views/StudentListView.vue
+++ src/views/StudentListView.vue
@@ -1,25 +1,36 @@
 <script setup lang="ts">
 import StudentCard from '@/components/StudentCard.vue'
 import type { StudentItem } from '@/type'
-import { ref, watchEffect } from 'vue'
+import { ref, watchEffect, computed } from 'vue'
 import type { Ref } from 'vue'
 import StudentService from '@/services/StudentService'
 
 
 const students: Ref<Array<StudentItem>> = ref([])
+const totalEvent = ref<number>(0)
 
 const props = defineProps({
   page: {
     type: Number,
     required: true
+  },
+  limit: {
+    type: Number,
+    required: true
   }
-  
 })
 
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvent.value / 2)
+  return props.page.valueOf() < totalPages
+})
+
+const limit = ref(props.limit)
+
 watchEffect(() => {
-  StudentService.getStudents(6, props.page).then((response) => {
+  StudentService.getStudents(2, props.page).then((response) => {
     students.value = response.data
-    console.log(response.data);
+    totalEvent.value = response.headers['x-total-count']
   })
 })
 
@@ -30,8 +41,18 @@ watchEffect(() => {
     <StudentCard  v-for="student in students"
       :key="student.id"
       :student="student"/>
+      <div class="pagination">
+      <RouterLink :to="{ name: 'student-list', query: { page: page - 1, limit: limit } }" rel="prev" v-if="page != 1"
+        id="page-prev"> Prev
+        Page
+      </RouterLink>
+      <RouterLink :to="{ name: 'student-list', query: { page: page + 1, limit: limit } }" rel="next" v-if="hasNextPage"
+        id="page-next">
+        Next Page
+      </RouterLink>
+    </div>
   </main>
-
+  
 </template>
 
 
