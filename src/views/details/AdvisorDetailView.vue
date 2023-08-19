71f3c80cb23a0c6b58e5cd35d242f45a298fb650
Fame <fame5454@gmail.com>
2023-08-20 05:51:31 +0700

    Edit advisordetail

--- src/views/details/AdvisorDetailView.vue
+++ src/views/details/AdvisorDetailView.vue
@@ -11,17 +11,13 @@ defineProps({
 </script>
 
 <template>
-  <div v-if="advisor">
+  <div v-if="advisor" class="flex justify-center items-center min-h-screen">
     <div class="card-wrapper">
       <div class="card-body">
-        <div class="card-body">
-          <div class="card-image">
-            <img :src="advisor?.image" alt="advisor image"/>
-          </div>
+        <div class="card-image">
+          <img :src="advisor?.image" alt="advisor image" class="rounded-lg shadow-md">
         </div>
-        <h2>
-          {{ advisor?.name }} {{ advisor?.surname }} 
-        </h2>
+        <h2 class="text-2xl font-semibold mb-2">{{ advisor?.name }} {{ advisor?.surname }}</h2>
       </div>
     </div>
   </div>
@@ -29,15 +25,27 @@ defineProps({
 
 <style scoped>
 .card-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  max-width: 600px;
+  margin: auto;
+  padding: 20px;
+  background-color: #f7fafc;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 }
+
 .card-body {
-  line-height: 36pt;
-  margin-top: 2rem;
+  text-align: center;
+  font-size: 18px;
+  line-height: 1.6;
 }
+
 span {
   font-weight: bold;
 }
+
+img {
+  max-width: 100%;
+  height: auto;
+}
 </style>
+
